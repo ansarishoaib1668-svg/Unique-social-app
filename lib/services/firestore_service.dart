f1db32ad3293e99ba04e26fa9144fdf94266c6b8
root <root@localhost.localdomain>
2026-07-21 22:52:30 +0530

    Add create post button

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,11 +1,15 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import '../models/post_model.dart';
 
 class FirestoreService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
 
   Stream<List<PostModel>> getPosts() {
-    return _db.collection('posts')
+    return _db
+        .collection('posts')
         .orderBy('createdAt', descending: true)
         .snapshots()
         .map((snapshot) {
@@ -18,6 +22,37 @@ class FirestoreService {
   Future<void> likePost(String postId) async {
     await _db.collection('posts').doc(postId).update({
       'likes': FieldValue.increment(1),
+    });
+  }
+
+  Future<void> addComment(String postId, String comment) async {
+    await _db.collection('posts').doc(postId).update({
+      'comments': FieldValue.arrayUnion([comment]),
+    });
+  }
+
+  Future<String> uploadFile(File file, String type) async {
+    final ref = _storage
+        .ref()
+        .child('posts/${DateTime.now().millisecondsSinceEpoch}.$type');
+
+    await ref.putFile(file);
+
+    return await ref.getDownloadURL();
+  }
+
+  Future<void> createPost({
+    required String text,
+    String? imageUrl,
+    String? videoUrl,
+  }) async {
+    await _db.collection('posts').add({
+      'text': text,
+      'imageUrl': imageUrl ?? '',
+      'videoUrl': videoUrl ?? '',
+      'likes': 0,
+      'comments': [],
+      'createdAt': FieldValue.serverTimestamp(),
     });
   }
 }
