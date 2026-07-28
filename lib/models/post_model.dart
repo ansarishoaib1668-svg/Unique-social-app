77de2c9ad3af43ae0f53259ffea86bac7cdf7138
root <root@localhost.localdomain>
2026-07-28 23:39:53 +0530

    Fix Firestore post comment parsing

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -16,13 +16,30 @@ class PostModel {
   });
 
   factory PostModel.fromMap(String id, Map<String, dynamic> map) {
+    final rawComments = map['comments'];
+
+    final List<String> parsedComments = [];
+
+    if (rawComments is List) {
+      for (final comment in rawComments) {
+        if (comment is String) {
+          parsedComments.add(comment);
+        } else if (comment is Map) {
+          final text = comment['text'];
+          if (text is String && text.isNotEmpty) {
+            parsedComments.add(text);
+          }
+        }
+      }
+    }
+
     return PostModel(
       id: id,
-      text: map['text'] ?? '',
-      likes: map['likes'] ?? 0,
-      imageUrl: map['imageUrl'] ?? '',
-      videoUrl: map['videoUrl'] ?? '',
-      comments: List<String>.from(map['comments'] ?? []),
+      text: map['text'] is String ? map['text'] : '',
+      likes: map['likes'] is int ? map['likes'] : 0,
+      imageUrl: map['imageUrl'] is String ? map['imageUrl'] : '',
+      videoUrl: map['videoUrl'] is String ? map['videoUrl'] : '',
+      comments: parsedComments,
     );
   }
 }
